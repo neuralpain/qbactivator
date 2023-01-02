c80b25d72cb4bda66fd79026839e66316183c5f1
neuralpain <77242216+neuralpain@users.noreply.github.com>
2023-01-01 21:45:41 -0400

    remove bin folder

--- QBTOOLHUB/Resource/CleanInstallTool.txt.bat
+++
@@ -1,39 +0,0 @@
-QBDataServiceUsertaskkill.bat„w
-
-taskkill.exe /IM qbw32.exe/F
-
-taskkill.exe /IM qbupdate.exe/F
-
-taskkill.exe /IM QBCFMonitorService.exe/F
-
-taskkill.exe /IM QBUpdateService.exe/F
-
-taskkill.exe /IM IntuitUpdateService.exe/F
-
-taskkill.exe /IM QBDBMgr.exe/F
-
-taskkill.exe /IM QBDBMgrN.exe/F
-
-taskkill.exe /IM QBSEVNT10R.exe/F
-
-taskkill.exe /IM QBWebConnector.exe/F
-
-taskkill.exe /IM QuickBooksMessaging.exe/F
-
-taskkill.exe /IM QBIDPservice.exe/F
-
-taskkill.exe /IM IntuitDataProtect.exe/F
-
-taskkill.exe /IM IntuitSyncManager.exe/F
-
-taskkill.exe/FI"imagenameeqQBDBMgrN.exe"/FI"usernameeq
-"/F
-
-cmd.exe/cQC:\ProgramData\Intuit\EntitlementClientAC:\ProgramData\Intuit\QuickBooksC:\Users\A\AppData\Local\Intuit\QuickBooksEC:\ProgramFiles\Intuit\QuickBooksQC:\ProgramFiles(x86)\Intuit\QuickBooks;\v8\EntitlementDataStore.ecml?\v6.0\EntitlementDataStore.ecml5C:\DocumentsandSettings\O\LocalSettings\ApplicationData\Intuit€C:\DocumentsandSettings\AllUsers\ApplicationData\Intuit\QuickBooksUThefollowingfoldershavebeenlocated:
-
-9
-
-Andhavebeenrenamedto:
-€
-
-PleaseinstallyourcopyofQuickBookstothedefaultlocation_MM.dd.hh.mm$this.IconqbCleaninstallePleaseselectyourversionofEnterpriseSolutions+EnterpriseSolutionsQPleaseselectyourversionofQuickBooksgrpVersion%QuickBooksVersionrdbEnterprise)EnterpriseSolutionsrdbProPremierPro/Premier9.08.07.0cmbEnterprisecmbPropremierbtnContinueContinuelblversion€§WeneedtoknowwhichversionofQuickBooksyourattemptingtoinstalltocontinue.lblRelease!ProductVersion:-FormCleanInstallManualAQuickBooksCleanInstallUtilityOClean_Install_Tool.Properties.Resourcesë'ÕnèG¯œ•2äÔÁ·z\V4à‰!%)-15959
--- QBTOOLHUB/Resource/sfcscannow.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-@setlocal enableextensions
-@cd /d "%~dp0"
-
-echo Running Microsoft SFC Scannow (Please Wait- DO NOT CLOSE THIS WINDOW!!!)...
-C:\windows\sysnative\sfc /scannow
-pause
-Dism /Online /Cleanup-Image /RestoreHealth
-pause
