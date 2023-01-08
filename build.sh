b0b0557d85ea830446fa5909a62f59dd068d8042
neuralpain <77242216+neuralpain@users.noreply.github.com>
2023-01-08 16:46:25 -0400

    Create build script

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+version=$(<VERSION)
+buildfile=./build/qbactivator.cmd
+
+case "$1" in
+  -i) buildfile=./dist/qbactivator.cmd
+    [ ! -d "./dist" ] && mkdir dist || rm -r ./dist/*;;
+  *) build="$(date "+$version.%y%m%d.%H%M%S")"
+    version="$version-beta [Build $build]"
+    buildfile=./build/qbactivator-$build.cmd
+    [ ! -d "./build" ] && mkdir build;;
+esac
+
+# script header
+echo "<# :# DO NOT REMOVE THIS LINE" > $buildfile
+echo >> $buildfile
+
+# script info
+echo ":: Name: qbactivator.cmd" >> $buildfile
+echo ":: Author: neuralpain" >> $buildfile
+echo ":: Version: $version" >> $buildfile
+echo ":: Description: Activation script body written in batch" >> $buildfile
+echo >> $buildfile
+
+# add main body
+echo '@set "uivr='$version'"' >> $buildfile
+cat ./src/main.cmd >> $buildfile
+echo >> $buildfile
+echo ":: export instructions to text file">> $buildfile
+echo ":qbreadme:">> $buildfile
+
+# add instructions
+cat ./src/qbreadme.min.md >> $buildfile
+echo ":qbreadme:" >> $buildfile
+echo >> $buildfile
+
+# add powershell functions
+echo "# ---------- powershell script ---------- #>" >> $buildfile
+echo >> $buildfile
+cat ./src/Invoke-LicenseInstallation.ps1 >> $buildfile
+
+printf "Created \"$buildfile.\"\n\n"
