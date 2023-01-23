3cb1df0fd7f537018fe55cc1497790e239b4bb18
neuralpain <77242216+neuralpain@users.noreply.github.com>
2023-01-22 23:21:15 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,54 +1,71 @@
 #!/bin/bash
 
-# Name: build.sh
-# Author: neuralpain
-# Version: 1.2
-# Description: build script for qbactivator.cmd
+# build.sh Version 1.3
+# Copyright (c) 2023, neuralpain
+# Build script for qbactivator.cmd
 
 version=$(<VERSION)
 buildfile=./build/qbactivator.cmd
 zipfile=qbactivator-$version.zip
+main=./src/main.cmd
+helpfile=./src/qbreadme.txt
+pwshscript=./src/ExecutionModule.ps1
 
 build() {
   # script header
   echo "<# :# DO NOT REMOVE THIS LINE" > $buildfile
   echo >> $buildfile
   # script info
-  echo ":: Name: qbactivator.cmd" >> $buildfile
-  echo ":: Author: neuralpain" >> $buildfile
-  echo ":: Version: $version" >> $buildfile
-  echo ":: Description: Activation script body written in batch" >> $buildfile
+  echo ":: qbactivator.cmd, Version $version" >> $buildfile
+  echo ":: Copyright (c) 2023, neuralpain" >> $buildfile
+  echo ":: Activation script body written in batch" >> $buildfile
   echo >> $buildfile
   # add main body
   echo '@set "uivr='$version'"' >> $buildfile
-  cat ./src/main.cmd >> $buildfile
+  cat $main >> $buildfile
   echo >> $buildfile
   # add instructions
   echo ":: export instructions to text file">> $buildfile
   echo ":qbreadme:">> $buildfile
-  cat ./src/qbreadme.min.md >> $buildfile
+  cat $helpfile >> $buildfile
   echo ":qbreadme:" >> $buildfile
   echo >> $buildfile
   # add powershell functions
   echo "# ---------- powershell script ---------- #>" >> $buildfile
   echo >> $buildfile
-  cat ./src/Invoke-LicenseInstallation.ps1 >> $buildfile
+  cat $pwshscript >> $buildfile
   printf "\033[1;32mBuild complete.\n\033[0m"
 }
 
 compress() {
+  cp ./LICENSE dist
   cd dist && zip -q $zipfile * &>/dev/null || ( printf "\033[0;31mFailed to archive files.\033[0m" && return )
+  rm ./LICENSE
   if [ -f $zipfile ]; then printf "\033[0;37mArchived to \"dist/$zipfile\"\n\033[0m"; fi
 }
 
+printusage() {
+  echo "Usage: build [OPTION]"
+  printf "Build script for qbactivator.cmd\n\n"
+  echo "  -i, --release      Build for stable release"
+  echo "  -t, --test         Build unit tests"
+  echo "  -C, --clear-all    Delete \"build\" and \"dist\" folder"
+  echo "  -c, --clear        Clear all unit test builds"
+  echo "  -h, --help         Display this help message" && echo
+}
+
 case "$1" in
-  -i) buildfile=./dist/qbactivator.cmd
-    [ ! -d "./dist" ] && mkdir dist || rm -r ./dist/*
+  -h|--help) printusage;;
+  -c|--clear) rm ./build/* &>/dev/null;;
+  -C|--clear-all) rm -r ./build ./dist &>/dev/null;;
+  -i|--release) buildfile=./dist/qbactivator.cmd
+    [ ! -d "./dist" ] && mkdir dist || rm ./dist/*
     cp ./src/qbpatch.dat ./dist
     build && compress;;
-  *) build="$(date "+$version.%y%m%d.%H%M%S")"
-    version="$version-beta [Build $build]"
+  -t|--test) build="$(date "+$version.%y%m%d%H%M%S")"
+    version="$version [Build $build]"
     buildfile=./build/qbactivator-$build.cmd
     [ ! -d "./build" ] && mkdir build
     build;;
+  *) printusage && exit;;
 esac
