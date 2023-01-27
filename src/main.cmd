1dc9a670242e847fc9300c9fb8e24abd7582b7c1
neuralpain <77242216+neuralpain@users.noreply.github.com>
2023-01-27 07:13:10 -0400

    Update main.cmd

--- src/main.cmd
+++ src/main.cmd
@@ -1,6 +1,5 @@
 @title qbactivator %uivr%
 @mode 60,16
-@echo off
 
 :: PowerShell config
 :# Disabling argument expansion avoids issues with ! in arguments.
@@ -18,7 +17,7 @@ set "QBPOSDIR11=C:\Program Files (x86)\Intuit\QuickBooks Point of Sale 11.0\QBPO
 set "QBPOSDIR12=C:\Program Files (x86)\Intuit\QuickBooks Point of Sale 12.0\QBPOSShell.exe"
 set "QBPOSDIR18=C:\Program Files (x86)\Intuit\QuickBooks Desktop Point of Sale 18.0\QBPOSShell.exe"
 set "QBPOSDIR19=C:\Program Files (x86)\Intuit\QuickBooks Desktop Point of Sale 19.0\QBPOSShell.exe"
-set "PATCHFOLDER=%SystemRoot%\Microsoft.NET\assembly\GAC_MSIL\Intuit.Spc.Map.EntitlementClient.Common\v4.0_8.0.0.0__5dc4fe72edbcacf5"
+set "CLIENTFOLDER=%SystemRoot%\Microsoft.NET\assembly\GAC_MSIL\Intuit.Spc.Map.EntitlementClient.Common\v4.0_8.0.0.0__5dc4fe72edbcacf5"
 
 :: check admin permissions
 fsutil dirty query %systemdrive% >nul
@@ -41,16 +40,16 @@ goto :eof
 
 :init
 cls & echo.
-echo Please wait...
-:: perform simple hashcheck for patch file integrity
-:: create folders for patch and start installer
-%pwsh% ^"Invoke-Expression ('^& {' + (get-content -raw '%~f0') + '} %ARGS%')"
+echo Initializing. Please wait...
+%pwsh% ^"Invoke-Expression ('^& {' + (Get-Content -Raw '%~f0') + '} %ARGS%')"
 if %ERRORLEVEL% EQU 3 (
   echo. & pause
-  goto :exitQBA 
-) else if %ERRORLEVEL% NEQ 0 (
-  goto :exitQBA
-) else ( goto :startQBA )
+  goto exitQBA
+) else if %ERRORLEVEL% EQU 2 (
+  goto :beginActivation
+) else if %ERRORLEVEL% EQU 0 (
+  goto :startQBA
+) else ( goto exitQBA ) 
 
 :startQBA
 :: start screen
@@ -64,7 +63,7 @@ echo installed before you continue. Continue when ready.
 echo.
 pause
 
-:: end QuickBooks background processes
+:beginActivation
 cls & echo.
 echo Terminating QB processes...
 taskkill /fi "imagename eq qb*" /f /t >nul 2>&1
@@ -86,33 +85,40 @@ taskkill /f /im QuickBooksMessaging.exe >nul 2>&1
 echo. & echo Done.
 
 :: insert patch file for activation
-ren "%PATCHFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll" "Intuit.Spc.Map.EntitlementClient.Common.dll.bak" >nul
-copy /v /y /z "%wdir%qbpatch.dat" "%PATCHFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll" >nul
+if exist "%CLIENTFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll" (
+  ren "%CLIENTFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll" "Intuit.Spc.Map.EntitlementClient.Common.dll.bak" >nul
+  copy /v /y /z "%wdir%qbpatch.dat" "%CLIENTFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll" >nul
 
-:: export and open minified readme
-pushd "%wdir%"
-@set "0=%~f0"
-%pwsh% "$f=[IO.File]::ReadAllText($env:0) -split ':qbreadme\:.*'; [IO.File]::WriteAllText('qbreadme.txt',$f[1].Trim(),[System.Text.Encoding]::UTF8)"
-popd & start notepad.exe "qbreadme.txt"
+  :: export and open minified readme
+  pushd "%wdir%"
+  @set "0=%~f0"
+  %pwsh% "$f=[IO.File]::ReadAllText($env:0) -split ':qbreadme\:.*'; [IO.File]::WriteAllText('qbreadme.txt',$f[1].Trim(),[System.Text.Encoding]::UTF8)"
+  popd & start notepad.exe "qbreadme.txt"
 
-cls & echo.
-echo Starting services...
-net start "Intuit Entitlement Service v8" >nul 2>&1
-net start "QBPOSDBServiceV11" >nul 2>&1
+  cls & echo.
+  echo Starting services...
+  net start "Intuit Entitlement Service v8" >nul 2>&1
+  net start "QBPOSDBServiceV11" >nul 2>&1
 
-:: start quickbooks
-cls & echo.
-echo Starting QuickBooks...
-if exist "%QBPOSDIR19%" (
-  %pwsh% "Start-Process -FilePath '%QBPOSDIR19%'"
-) else if exist "%QBPOSDIR18%" (
-  %pwsh% "Start-Process -FilePath '%QBPOSDIR18%'"
-) else if exist "%QBPOSDIR12%" (
-  %pwsh% "Start-Process -FilePath '%QBPOSDIR12%'"
-) else if exist "%QBPOSDIR11%" (
-  %pwsh% "Start-Process -FilePath '%QBPOSDIR11%'"
+  :: start quickbooks
+  cls & echo.
+  echo Starting QuickBooks...
+  if exist "%QBPOSDIR19%" (
+    %pwsh% "Start-Process -FilePath '%QBPOSDIR19%'"
+  ) else if exist "%QBPOSDIR18%" (
+    %pwsh% "Start-Process -FilePath '%QBPOSDIR18%'"
+  ) else if exist "%QBPOSDIR12%" (
+    %pwsh% "Start-Process -FilePath '%QBPOSDIR12%'"
+  ) else if exist "%QBPOSDIR11%" (
+    %pwsh% "Start-Process -FilePath '%QBPOSDIR11%'"
+  )
+  ping -n 3 127.0.0.1 >nul
+) else (
+  cls & echo.
+  echo QuickBooks was not found on the system.
+  ping -n 3 127.0.0.1 >nul
+  goto exitQBA
 )
-ping -n 3 127.0.0.1 >nul
 
 cls & echo.
 echo Follow the steps below to activate QuickBooks software.
@@ -122,7 +128,7 @@ echo -- DO NOT check for updates
 echo 2. Click "Remind me later"
 echo 3. Click "Help" then "Registration"
 echo 4. Click "Register by phone now"
-echo 5. Enter the code 99999930
+echo 5. Enter the code 999999
 echo 6. Click Next
 echo 7. Click Finish
 echo.
@@ -153,15 +159,15 @@ echo. & echo Done.
 :: restore files to original state
 cls & echo.
 echo Attempting to restore client module...
-copy /v /y /z "%PATCHFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll.bak" "%PATCHFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll" >nul
-fc "%PATCHFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll" "%PATCHFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll.bak" > nul
+copy /v /y /z "%CLIENTFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll.bak" "%CLIENTFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll" >nul
+fc "%CLIENTFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll" "%CLIENTFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll.bak" > nul
 
 :: error handling if files have not been restored
 if %ERRORLEVEL% EQU 1 (
   cls & echo.
   echo The script ran into a problem while attempting to restore the file.
   echo. & pause
-  explorer.exe %PATCHFOLDER%
+  explorer.exe %CLIENTFOLDER%
   cls & echo.
   echo Do the following to resolve the error:
   echo 1. Delete "Intuit.Spc.Map.EntitlementClient.Common.dll"
@@ -169,7 +175,7 @@ if %ERRORLEVEL% EQU 1 (
   echo. & pause
   goto :exitQBA
 ) else (
-  del /q /f /a "%PATCHFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll.bak"
+  del /q /f /a "%CLIENTFOLDER%\Intuit.Spc.Map.EntitlementClient.Common.dll.bak"
   cls & echo.
   echo Patch completed without errors.
   ping -n 2 127.0.0.1 >nul
