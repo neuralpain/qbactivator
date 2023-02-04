a992fb2528bc4cf4829d11c13f0929148b3086a4
neuralpain <77242216+neuralpain@users.noreply.github.com>
2023-02-04 17:17:56 -0400

    Update build script
    New: Export standalone archive for lightweight release

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# build.sh Version 1.3
+# build.sh Version 1.4
 # Copyright (c) 2023, neuralpain
 # Build script for qbactivator.cmd
 
 version=$(<VERSION)
-buildfile=./build/qbactivator.cmd
+name=qbactivator.cmd
+buildfile=./build/$name
 zipfile=qbactivator-$version.zip
-main=./src/main.cmd
-helpfile=./src/qbreadme.txt
-pwshscript=./src/ExecutionModule.ps1
+zipfile2=qbactivator-$version-standalone.zip
+main=./src/main/main.cmd
+patchfile=./src/main/qbpatch.dat
+helpfile=./src/main/qbreadme.txt
+pwshscript=./src/main/ExecutionModule.ps1
 
 build() {
   # script header
@@ -39,9 +42,9 @@ build() {
 }
 
 compress() {
-  cp ./LICENSE dist
-  cd dist && zip -q $zipfile * &>/dev/null || ( echo -e "\033[0;31mE:\033[0m Failed to create archive." && return )
-  rm ./LICENSE
+  cp ./LICENSE ./VERSION $patchfile dist && cd dist 
+  ( zip -q $zipfile * && zip -q $zipfile2 * -x qbpatch.dat *.zip ) || ( echo -e "\033[0;31mE:\033[0m Failed to create archive." && return )
+  rm ./LICENSE ./VERSION ./qbpatch.dat
   if [ -f $zipfile ]; then echo -e "\033[0;37mArchived to \"dist/$zipfile\"\033[0m"; fi
 }
 
@@ -60,13 +63,12 @@ case "$1" in
   -c|--clear) rm ./build/* &>/dev/null;;
   -C|--clear-all) rm -r ./build ./dist &>/dev/null;;
   -i|--release) buildfile=./dist/qbactivator.cmd
-    [ ! -d "./dist" ] && mkdir dist || rm ./dist/*
-    cp ./src/qbpatch.dat ./dist
+    [[ ! -d "./dist" ]] && mkdir dist || rm ./dist/*
     build && compress;;
   -t|--test) build="$(date "+$version.%y%m%d%H%M%S")"
     version="$version [Build $build]"
     buildfile=./build/qbactivator-$build.cmd
-    [ ! -d "./build" ] && mkdir build
-    build;;
+    [[ ! -d "./build" ]] && mkdir build
+    build && exit;;
   *) printusage && exit;;
 esac
