921bb20bfc3be54e10746900f482d3800cd1c4c9
neuralpain <77242216+neuralpain@users.noreply.github.com>
2023-08-12 22:39:09 -0400

    Update tests

--- src/tests/utf8-noBOM.ps1
+++
@@ -1,7 +0,0 @@
-$speedtestclifolder = "$env:APPDATA\Ookla\Speedtest CLI"
-$speedtestclilicense = "[Settings]`r`nLicenseAccepted=604ec27f828456331ebf441826292c49276bd3c1bee1a2f65a6452f505c4061c"
-
-if (-not(Test-Path -Path "$speedtestclifolder\speedtest-cli.ini" -PathType Leaf)) {
-  mkdir $speedtestclifolder >$null 2>&1
-  [IO.File]::WriteAllLines("$speedtestclifolder\speedtest-cli.ini",$speedtestclilicense)
-}
--- src/tests/zipextracttest.ps1
+++
@@ -1,9 +0,0 @@
-# $speedtestarchive = "https://github.com/neuralpain/qbactivator/files/10474537/ookla-speedtest-1.2.0-win64.zip"
-# Start-BitsTransfer $speedtestarchive ".\speedtest.zip"
-
-Add-Type -Assembly System.IO.Compression.FileSystem
-$zipFile = [IO.Compression.ZipFile]::OpenRead("$pwd\speedtest.zip")
-[System.IO.Compression.ZipFileExtensions]::ExtractToFile(
-  ($zipFile.Entries | where {$_.Name -eq "speedtest.exe"}),
-  "$pwd\speedtest.exe", $true)
-$zipFile.Dispose()
