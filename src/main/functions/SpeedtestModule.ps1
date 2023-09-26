6567983c8ad27b9ffc9d77262f1e1fe028408ee2
neuralpain <77242216+neuralpain@users.noreply.github.com>
2023-09-26 10:21:49 -0400

    speedtest bug fix
    Fix for "Cannot convert value" and "Attempted to divide by zero" error.

--- src/main/functions/SpeedtestModule.ps1
+++ src/main/functions/SpeedtestModule.ps1
@@ -1,18 +1,21 @@
-# add speedtest cli license
-$speedtest_folder = "$env:APPDATA\Ookla\Speedtest CLI"
-$speedtest_license_file = "$env:APPDATA\Ookla\Speedtest CLI\speedtest-cli.ini"
-$speedtest_license_text = "[Settings]`r`nLicenseAccepted=604ec27f828456331ebf441826292c49276bd3c1bee1a2f65a6452f505c4061c" # utf8, noBOM
+<#
+  # add speedtest cli license
+  $speedtest_folder = "$env:APPDATA\Ookla\Speedtest CLI"
+  $speedtest_license_file = "$env:APPDATA\Ookla\Speedtest CLI\speedtest-cli.ini"
+  $speedtest_license_text = "[Settings]`r`nLicenseAccepted=604ec27f828456331ebf441826292c49276bd3c1bee1a2f65a6452f505c4061c"  utf8, noBOM
 
-if (-not(Test-Path "$speedtest_license_file" -PathType Leaf)) {
-  mkdir $speedtest_folder >$null 2>&1 
-  [IO.File]::WriteAllLines($speedtest_license_file, $speedtest_license_text)
-}
+  if (-not(Test-Path "$speedtest_license_file" -PathType Leaf)) {
+    mkdir $speedtest_folder >$null 2>&1 
+    [IO.File]::WriteAllLines($speedtest_license_file, $speedtest_license_text)
+  }
+#>
 
 function Get-SpeedTestResults {
   $speedtest_archive = "https://github.com/neuralpain/qbactivator/files/11450516/speedtest.zip"
   
   # don't waste time downloading the cli again if it's available
   if (-not(Test-Path "$qbactivator_temp\speedtest.exe")) {
+    # download and extract the cli from the archive
     Start-BitsTransfer $speedtest_archive "$qbactivator_temp\speedtest.zip"
     Add-Type -Assembly System.IO.Compression.FileSystem
     $_zip = [IO.Compression.ZipFile]::OpenRead("$qbactivator_temp\speedtest.zip")
@@ -22,9 +25,11 @@ function Get-SpeedTestResults {
     $_zip.Dispose()
   }
 
-  [int]$speedtestresult = [math]::Round((((Invoke-Expression "$qbactivator_temp\speedtest.exe --accept-gdpr --format json --progress no"
+  # accept CLI usage license and General Data Protection Regulation (EU) before use
+  Invoke-Expression "$qbactivator_temp\speedtest.exe --accept-gdpr --accept-license" >$null 2>&1
+  # start testing the internet speed
+  [int]$speedtestresult = [math]::Round((((Invoke-Expression "$qbactivator_temp\speedtest.exe --format json --progress no"
         ) -replace ".*download").Trim(':{"bandwidth":') -replace ",.*") / $BYTE_TO_MEGABYTE, 2)
-
   return [int]$speedtestresult
 }
 
