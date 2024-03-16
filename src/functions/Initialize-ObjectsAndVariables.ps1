db38dbb89a4892a7d486ca38a4c373604805b5e2
neuralpain <77242216+neuralpain@users.noreply.github.com>
2024-03-17 01:04:03 +0800

    Update Initialize-ObjectsAndVariables.ps1
    - Update log file name
    - Remove `$BYTE_TO_MEGABYTE`

--- src/functions/Initialize-ObjectsAndVariables.ps1
+++ src/functions/Initialize-ObjectsAndVariables.ps1
@@ -16,8 +16,6 @@ $TIME_SHORT = 800
 $TIME_NORMAL = 1000
 $TIME_SLOW = 2000
 
-$BYTE_TO_MEGABYTE = 1048576
-
 class Installer {
   [string] $Name
   [string] $VerNum
@@ -33,59 +31,59 @@ class Installer {
 }
 
 $POS11InstObj = [Installer]@{
-  Name = 'QuickBooksPOSV11.exe'
+  Name   = 'QuickBooksPOSV11.exe'
   VerNum = '11'
-  Year = '2013'
-  Hash = 'A1AF552A49ADFF40E6462A968DD552A4'
-  Path = 'Intuit\QuickBooks Point of Sale 11.0'
-  LNum1 = '1063-0575-1585-222'
-  LNum2 = '8432-0480-0178-029'
-  PNum = '023-147'
-  XBits = 164665944
-  XByte = 20583243
-  Size = '157.03'
+  Year   = '2013'
+  Hash   = 'A1AF552A49ADFF40E6462A968DD552A4'
+  Path   = 'Intuit\QuickBooks Point of Sale 11.0'
+  LNum1  = '1063-0575-1585-222'
+  LNum2  = '8432-0480-0178-029'
+  PNum   = '023-147'
+  XBits  = 164665944
+  XByte  = 20583243
+  Size   = '157.03'
 }
 
 $POS12InstObj = [Installer]@{
-  Name = 'QuickBooksPOSV12.exe'
+  Name   = 'QuickBooksPOSV12.exe'
   VerNum = '12'
-  Year = '2015'
-  Hash = '30FB99C5E98DF6874D438C478314EF9D'
-  Path = 'Intuit\QuickBooks Point of Sale 12.0'
-  LNum1 = '6740-7656-8840-594'
-  LNum2 = '0877-0442-6111-615'
-  PNum = '448-229'
-  XBits = 461077328
-  XByte = 57634666
-  Size = '439.71'
+  Year   = '2015'
+  Hash   = '30FB99C5E98DF6874D438C478314EF9D'
+  Path   = 'Intuit\QuickBooks Point of Sale 12.0'
+  LNum1  = '6740-7656-8840-594'
+  LNum2  = '0877-0442-6111-615'
+  PNum   = '448-229'
+  XBits  = 461077328
+  XByte  = 57634666
+  Size   = '439.71'
 }
 
 $POS18InstObj = [Installer]@{
-  Name = 'QuickBooksPOSV18.exe'
+  Name   = 'QuickBooksPOSV18.exe'
   VerNum = '18'
-  Year = '2018'
-  Hash = 'DD45AA4EC0DF431243C9836816E2305A'
-  Path = 'Intuit\QuickBooks Desktop Point of Sale 18.0'
-  LNum1 = '2421-4122-2213-596'
-  LNum2 = '3130-3560-7860-900'
-  PNum = '818-769'
-  XBits = 637981160
-  XByte = 79747645
-  Size = '608.42'
+  Year   = '2018'
+  Hash   = 'DD45AA4EC0DF431243C9836816E2305A'
+  Path   = 'Intuit\QuickBooks Desktop Point of Sale 18.0'
+  LNum1  = '2421-4122-2213-596'
+  LNum2  = '3130-3560-7860-900'
+  PNum   = '818-769'
+  XBits  = 637981160
+  XByte  = 79747645
+  Size   = '608.42'
 }
 
 $POS19InstObj = [Installer]@{
-  Name = 'QuickBooksPOSV19.exe'
+  Name   = 'QuickBooksPOSV19.exe'
   VerNum = '19'
-  Year = '2019'
-  Hash = 'F5C434677270319F9A515210CA916187'
-  Path = 'Intuit\QuickBooks Desktop Point of Sale 19.0'
-  LNum1 = '0106-3903-4389-908'
-  LNum2 = '7447-0864-8898-657'
-  PNum = '595-828'
-  XBits = 1017415680
-  XByte = 127176960
-  Size = '970.28'
+  Year   = '2019'
+  Hash   = 'F5C434677270319F9A515210CA916187'
+  Path   = 'Intuit\QuickBooks Desktop Point of Sale 19.0'
+  LNum1  = '0106-3903-4389-908'
+  LNum2  = '7447-0864-8898-657'
+  PNum   = '595-828'
+  XBits  = 1017415680
+  XByte  = 127176960
+  Size   = '970.28'
 }
 
 # start list from most recent version first
@@ -119,7 +117,7 @@ function Get-Version { return $Script:QB_VERSION }
 function Get-License { return $Script:LICENSE_KEY }
 
 # log file
-$LOG = "C:\Windows\Logs\qbactivator\QuickBooksActivation_Log_$(Get-Date -Format "yyyyMMdd_HHmmss").txt"
+$LOG = "C:\Windows\Logs\qbactivator\qbactivator_$(Get-Date -Format "yyyyMMdd_HHmmss").log"
 # add temp folder for qbactivator
 $qbactivator_temp = "$env:TEMP\qbactivator_temp"
 if (-not(Test-Path $qbactivator_temp)) { mkdir $qbactivator_temp >$null 2>&1 }
