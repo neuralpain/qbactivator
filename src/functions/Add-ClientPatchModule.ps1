d78e0165c3c6909b4ab280724d39bd988cd04594
neuralpain <77242216+neuralpain@users.noreply.github.com>
2024-03-30 13:22:39 +0800

    Update Add-ClientPatchModule.ps1

--- src/functions/Add-ClientPatchModule.ps1
+++ src/functions/Add-ClientPatchModule.ps1
@@ -1,59 +1,62 @@
 <# --- CLIENT MODULE FUNCTIONS --- #>
 
+function Get-ClientFileFromHost {
+  param(
+    [Parameter(Mandatory = $true)][String]$File,
+    # [Parameter(Mandatory = $true)][String]$Destination,
+    [Parameter(Mandatory = $true)][String]$Type
+  )
+  
+  $client_file_download_job = Start-Job -ScriptBlock {
+    param($_File)
+    Invoke-WebRequest -Uri $_File -OutFile $env:TEMP\EntClient.tmp
+    Copy-Item $env:TEMP\EntClient.tmp $CLIENT_MODULE_FULL_PATH
+    Remove-Item $env:TEMP\EntClient.tmp -Force
+  } -ArgumentList $File
+
+  Show-WebRequestDownloadJobState -DownloadJob $client_file_download_job -Message "Downloading client module"
+  # Wait-Job $client_file_download_job >$null 2>&1
+  # Remove-Job $client_file_download_job
+}
+
 function Get-ClientModule {
-  param($ClientModule)
+  param (
+    [String]$Local = $null, # initialaizad with $null to be validated by the if statement
+    [Parameter(Mandatory = $true)][String]$FromHost,
+    [Parameter(Mandatory = $true)][String]$Type
+  )
+
+  if ($Local) {
+    if (Test-Path $Local -PathType Leaf) {
+      Copy-Item $Local $CLIENT_MODULE_FULL_PATH
+      return
+    }
+  }
+  else { Write-Host "Local file not found." }
+
+  ### FALLBACK ###
+
+  # if its a host request only
   Write-Host -NoNewline "Testing connectivity... "
   if (-not(Test-Connection www.google.com -Quiet)) { 
     Write-Error_NoInternetConnectivity
   }
-  else {
-    Write-Host "OK"
-    Write-Host -NoNewLine "Downloading client module... "
-    Start-BitsTransfer $ClientModule $CLIENT_MODULE_PATH
-    Write-Host "Done"
-  }
+ 
+  Write-Host "OK"
+  # Get-ClientFileFromHost -File $FromHost -Destination $CLIENT_MODULE_FULL_PATH -Type $Type
+  
+  $client_file_download_job = Start-Job -ScriptBlock {
+    param($url)
+    Invoke-WebRequest -Uri $url -OutFile $CLIENT_MODULE_FULL_PATH
+  } -ArgumentList $FromHost
+  
+  Show-WebRequestDownloadJobState -DownloadJob $client_file_download_job -Message "Downloading client module"
+  return
 }
 
 function Find-GenuineClientModule {
-  if (-not(Test-Path $CLIENT_MODULE_PATH -PathType Leaf)) {
+  if (-not(Test-Path $CLIENT_MODULE_FULL_PATH -PathType Leaf)) {
     Write-Error_QuickBooksNotInstalled
-  }
-}
-
-function Repair-GenuineClientModule {
-  param([Switch]$SanityCheck)
-  
-  if (-not(Test-Path $CLIENT_MODULE_PATH -PathType Leaf)) {
-    Write-Error_QuickBooksNotInstalled # or file missing ### add this next
-  }
-
-  if ($SanityCheck) {
-    if ((Compare-IsValidHash $CLIENT_MODULE_PATH -Hash $PATCH_HASH)) {
-      Copy-Item $LOCAL_GENUINE_FILE $CLIENT_MODULE_PATH
-      else { Get-ClientModule $GENUINE_CLIENT_FILE_ON_HOST }
-    }
-    else { 
-      Write-Host "No issues found. Nothing to repair." 
-      Start-Sleep -Milliseconds $TIME_NORMAL
-      return
-    }
-    Write-Host "Client module repaired."
-    Start-Sleep -Milliseconds $TIME_NORMAL
-    return
-  }
-  
-  if (Test-Path "${CLIENT_MODULE_PATH}.bak" -PathType Leaf) {
-    Write-Host -NoNewLine "Fixing error on client module... "
-    Remove-Item $CLIENT_MODULE_PATH -Force >$null 2>&1
-    Rename-Item "${CLIENT_MODULE_PATH}.bak" $CLIENT_MODULE_PATH
-    Write-Host "Done"
-
-    $query = Read-Host "Continue to patch QuickBooks POS? (Y/n)"
-    
-    switch ($query) { 
-      "n" { exit $NONE } 
-      default { break } 
-    }
   }
 }
 
