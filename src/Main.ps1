4088319b9b6445b6ea9ebaaddbccd2d79dab987e
neuralpain <77242216+neuralpain@users.noreply.github.com>
2024-06-20 21:08:23 +0800

    Patch no-internet-download-loop bug
    This bug was found while testing the toast message feature during download. Coincidentally, the internet was disabled as a result of prior tests and a download could not be initiated.
    
    The result of this bug is a loop at `if ($null -ne $Script:QB_VERSION)` when it checks if a version has already been selected, which would always returns yes after failing to download.
    
    Bug fixed by making the script check for an internet connection before every download and return an error if failed.

--- src/Main.ps1
+++ src/Main.ps1
@@ -14,6 +14,7 @@ function Invoke-NextProcess {
         break
       }
       else {
+        &$TestInternetAvailable
         Start-InstallerDownload
         Invoke-NextProcess $PROC_LICENSE
       }
