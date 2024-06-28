1023ef1f3a31fb74c70aeed42d5018816305dd26
neuralpain <77242216+neuralpain@users.noreply.github.com>
2024-06-29 00:29:26 +0800

    create objects for all quickbooks desktop products
    Objects for QBDT Pro, Premier, Enterprise, and Accountant variants were created. These were all grouped together in respective files within an `InstallerObjects` folder. No changes were made to the POS objects.

--- src/functions/init/InstallerObjectClass.ps1
+++ src/functions/init/InstallerObjectClass.ps1
@@ -1,17 +1,20 @@
 class Installer {
-  [string] $WindowTitle      # installer window data
-  [System.Object] $Object    # POS Installer Object
-  [string] $Url              # download url
-  [string] $Name             # name of installer
-  [string] $VerNum           # POS version number, e.g. 11 
-  [string] $Year             # POS release year, e.g. 2013
-  [string] $Hash             # installer hash value
-  [string] $Path             # insatllation path
-  [string] $LNum1            # license number #1
-  [string] $LNum2            # license number #2
-  [string] $PNum             # product number
-  [string] $Validation       # validation string
-  [int]    $XBits            # installer bit size
-  [int]    $XByte            # installer byte size
-  [string] $Size             # installer megabyte size
+  [string] $ProductName         # quickbooks product name
+  [System.Object] $Object       # POS Installer Object
+  [string] $Url                 # download url
+  [string] $Name                # name of installer
+  [string] $Flavor              # flavor of quickbooks product, i.e., pro, bel, etc.
+  [string] $VerNum              # POS version number, e.g. 11
+  [string] $Year                # POS release year, e.g. 2013
+  [string] $Hash                # installer hash value
+  [string] $Path                # insatllation path
+  [string] $LNum1               # license number #1
+  [string] $LNum2               # license number #2, if available
+  [string] $PNum                # product number
+  [string] $Validation          # validation code
+  [string] $PropertyCode        # property code
+  [string] $ValidationMulti     # validation for 300 users
+  [int]    $XBits               # installer bit size
+  [int]    $XByte               # installer byte size
+  [string] $Size                # installer megabyte size
 }
