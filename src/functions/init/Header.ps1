9328d0350fd2ee80579df5181e92fbb153d392d6
neuralpain <77242216+neuralpain@users.noreply.github.com>
2024-07-03 23:54:17 +0800

    remove todo comment block

--- src/functions/init/Header.ps1
+++ src/functions/init/Header.ps1
@@ -3,8 +3,3 @@
   Copyright (c) 2023, neuralpain
   Module for activation of QuickBooks Point of Sale
 #>
-
-<#
-  [ ] check is quickbooks is installed
-  [ ] during activation, reset header screen to show "installing" status
-#>
